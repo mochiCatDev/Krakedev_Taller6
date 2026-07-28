86a7e7e7bdeca8720e0a18f614ed1969726ea90b
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-28 18:50:09 -0500

    feat: crear clase llamada Item con atributos nombre, productosActuales, productosDevueltos y productosVendidos.

---
+++ src/com/krakedev/repaso/entidades/Item.java
@@ -0,0 +1,8 @@
+package com.krakedev.repaso.entidades;
+
+public class Item {
+    private String nombre;
+    private int productosActuales;
+    private int productosDevueltos;
+    private int productosVendidos;
+}
