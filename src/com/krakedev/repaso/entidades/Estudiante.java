da7eb194c6f7f90c307b153d3dc3d09f5e210412
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-27 21:12:15 -0500

    feat: agregar constructor que recibe el nombre y lo asigna al atributo nombre

--- src/com/krakedev/repaso/entidades/Estudiante.java
+++ src/com/krakedev/repaso/entidades/Estudiante.java
@@ -4,4 +4,8 @@ public class Estudiante {
     private String nombre;
     private double nota;
     private String resultado;
+
+    public Estudiante(String nombre) {
+        this.nombre = nombre;
+    }
 }
