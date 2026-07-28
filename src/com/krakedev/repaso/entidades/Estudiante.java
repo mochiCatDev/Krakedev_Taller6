ad2f96392eac064fdd61edfc2595ef55b3791623
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-27 21:16:19 -0500

    feat: crear metodo calificar que recibe como parametro la nota
    el metodo recibe la nota y la asigna al atributo nota, también valida si la nota es menor, mayor o igual a 8 para poner una A o F al atributo resultado

--- src/com/krakedev/repaso/entidades/Estudiante.java
+++ src/com/krakedev/repaso/entidades/Estudiante.java
@@ -8,4 +8,13 @@ public class Estudiante {
     public Estudiante(String nombre) {
         this.nombre = nombre;
     }
+
+    public void calificar(double nota) {
+        this.nota = nota;
+        if (nota < 8) {
+            resultado = "F";
+        } else {
+            resultado = "A";
+        }
+    }
 }
