976d178b07879ddfb7e24c47365480443fbfd6d4
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-28 18:56:28 -0500

    feat: agregar métodos getters y setters para los atributos privados de la clase Item

--- src/com/krakedev/repaso/entidades/Item.java
+++ src/com/krakedev/repaso/entidades/Item.java
@@ -5,4 +5,16 @@ public class Item {
     private int productosActuales;
     private int productosDevueltos;
     private int productosVendidos;
+
+    // getters
+    public String getNombre() { return nombre; }
+    public int getProductosActuales() { return productosActuales; }
+    public int getProductosDevueltos() { return productosDevueltos; }
+    public int getProductosVendidos() { return productosVendidos; }
+
+    // setters
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setProductosActuales(int productosActuales) { this.productosActuales = productosActuales; }
+    public void setProductosDevueltos(int productosDevueltos) { this.productosDevueltos = productosDevueltos; }
+    public void setProductosVendidos(int productosVendidos) { this.productosVendidos = productosVendidos; }
 }
