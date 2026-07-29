393c82e9f0a49329ba611af0888bdde4e15a7359
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-28 19:47:30 -0500

    feat: integrar setters y getters en la clase Estudiante.

--- src/com/krakedev/repaso/entidades/Estudiante.java
+++ src/com/krakedev/repaso/entidades/Estudiante.java
@@ -6,7 +6,14 @@ public class Estudiante {
     private String resultado;
 
     // getters
+    public String getNombre() { return nombre; }
+    public double getNota() { return nota; }
     public String getResultado() { return resultado; }
+
+    // setters
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setNota(double nota) { this.nota = nota; }
+    public void setResultado(String resultado) { this.resultado = resultado; }
 
     public Estudiante(String nombre) {
         this.nombre = nombre;
