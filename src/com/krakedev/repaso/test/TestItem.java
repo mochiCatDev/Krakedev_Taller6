50101ac30133ad33e5366406d731910877ca69f6
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-28 19:32:32 -0500

    test: instanciar 2 objectos mas para testear los métodos

--- src/com/krakedev/repaso/test/TestItem.java
+++ src/com/krakedev/repaso/test/TestItem.java
@@ -3,8 +3,19 @@ import com.krakedev.repaso.entidades.Item;
 
 public class TestItem {
     public static void main(String[] args) {
-        Item item1 = new Item("Carro", 20);
+        Item item1 = new Item("Carro de juguete", 20);
+        Item item2 = new Item("Muñecos", 80);
+        Item item3 = new Item("Cuadernos", 42);
+
         item1.vender(8);
         item1.imprimir();
+
+        item2.vender(22);
+        item2.imprimir();
+
+        item3.vender(28);
+        item3.imprimir();
+        item3.vender(12);
+        item3.imprimir();
     }
 }
