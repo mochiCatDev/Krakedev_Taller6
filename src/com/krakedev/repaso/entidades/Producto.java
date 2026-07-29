abe04ffa0e41201a3f29171cb81c7690eb5b672a
mochiCatDev <230013094+mochiCatDev@users.noreply.github.com>
2026-07-28 19:51:55 -0500

    feat: integrar setters y getters en la clase Producto.

--- src/com/krakedev/repaso/entidades/Producto.java
+++ src/com/krakedev/repaso/entidades/Producto.java
@@ -4,16 +4,24 @@ public class Producto {
     private String nombre;
     private double precio;
 
+    // constructores
     public Producto(String nombre, double precio) {
         this.nombre = nombre;
         this.precio = precio;
     }
 
+    // getters
+    public String getNombre() { return nombre; }
+    public double getPrecio() { return precio; }
+
+    // setters
+    public void setNombre(String nombre) { this.nombre = nombre; }
     public void setPrecio(double precio) {
         if(precio < 0) { this.precio = precio * -1; }
         else if(precio > 0) { this.precio = precio; }
     }
 
+    // métodos
     public double calcularPrecioPromo(double porcentajeDescuento) {
         if(porcentajeDescuento >= 0 && porcentajeDescuento <= 100) {
             precio = precio * (100 - porcentajeDescuento) / 100;
